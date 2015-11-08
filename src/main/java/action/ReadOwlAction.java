ca9f2943249f48ab614071c6c8900ce55754b9e3
041784 <041784@041784-HP>
2015-11-08 13:02:06 -0200

    Correção do mapeamento do FrontController. Leitura do OWL no ReadOWLAction . Adicionado form e função load OWL em HTML.

--- src/main/java/action/ReadOwlAction.java
+++ src/main/java/action/ReadOwlAction.java
@@ -1,18 +1,27 @@
 package action;
 
+import config.Ontology;
 import controller.Action;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntResource;
+import org.apache.jena.util.iterator.ExtendedIterator;
 
 public class ReadOwlAction extends Action {
 
     //private ArrayList<String> 
-    
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         //
-        
+        ExtendedIterator<Individual> individuals = Ontology.getInstance().getModel().listIndividuals();
+        while (individuals.hasNext()) {
+            OntResource individuo = individuals.next();
+            String line = individuo.getLocalName();
+            //resposta.add(line);
+        }
+
     }
 
 }
