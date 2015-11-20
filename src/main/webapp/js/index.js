524389c55c4b5885d9270aebd162c5fd1dec43ce
041784 <041784@10.13.2.32>
2015-11-20 16:50:35 -0200

    Inserção de Imagens no SVG.

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,91 +1,34 @@
+$(window).resize(function () {
+    var width = $(window).width() - 18;
+    var height = $(window).height() - ($("#config").height() + 32);
+    $("#graph").width(width)
+            .height(height);
+    d3.select("svg")
+            .attr("width", width)
+            .attr("height", height);
+});
+
 $(document).ready(function () {
-    $.post("FrontController?action=ReadOwl", function (json) {
-        var links = json;
-        var nodes = {};
+    $(window).resize();
 
-        var winWidth = $(window).width() - 18;
-        var winHight = $(window).height() - ($("#config").height() + 32);
-        $("#graph").width(winWidth)
-                .height(winHight);
+    var svg = d3.select("#graph")
+            .append("svg");
+    var nodes;
+    var links;
 
-// Compute the distinct nodes from the links.
-        links.forEach(function (link) {
-            link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-            link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-        });
+    //$.post("FrontController?action=ReadNodes", function (json) {
+    nodes = [{x: 30, y: 50},
+        {x: 50, y: 80},
+        {x: 90, y: 120}];
+    //alert(json);
+    //}, "json");
 
-        var width = winWidth, height = winHight;
-
-        var force = d3.layout.force()
-                .nodes(d3.values(nodes))
-                .links(links)
-                .size([width, height])
-                .linkDistance(60)
-                .charge(-300)
-                .on("tick", tick)
-                .start();
-
-        var svg = d3.select("#graph").append("svg")
-                .attr("width", width)
-                .attr("height", height);
-
-// Per-type markers, as they don't inherit styles.
-        svg.append("defs").selectAll("marker")
-                .data(["suit", "licensing", "resolved"])
-                .enter().append("marker")
-                .attr("id", function (d) {
-                    return d;
-                })
-                .attr("viewBox", "0 -5 10 10")
-                .attr("refX", 15)
-                .attr("refY", -1.5)
-                .attr("markerWidth", 6)
-                .attr("markerHeight", 6)
-                .attr("orient", "auto")
-                .append("path")
-                .attr("d", "M0,-5L10,0L0,5");
-
-        var path = svg.append("g").selectAll("path")
-                .data(force.links())
-                .enter().append("path")
-                .attr("class", function (d) {
-                    return "link " + d.type;
-                })
-                .attr("marker-end", function (d) {
-                    return "url(#" + d.type + ")";
-                });
-
-        var circle = svg.append("g").selectAll("circle")
-                .data(force.nodes())
-                .enter().append("circle")
-                .attr("r", 8)
-                .call(force.drag);
-
-        var text = svg.append("g").selectAll("text")
-                .data(force.nodes())
-                .enter().append("text")
-                .attr("x", 8)
-                .attr("y", ".31em")
-                .text(function (d) {
-                    return d.name;
-                });
-
-// Use elliptical arc path segments to doubly-encode directionality.
-        function tick() {
-            path.attr("d", linkArc);
-            circle.attr("transform", transform);
-            text.attr("transform", transform);
-        }
-
-        function linkArc(d) {
-            var dx = d.target.x - d.source.x,
-                    dy = d.target.y - d.source.y,
-                    dr = Math.sqrt(dx * dx + dy * dy);
-            return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-        }
-
-        function transform(d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        }
-    }, "json");
+    svg.selectAll("image")
+            .data(nodes)
+            .enter()
+            .append("image")
+            .data(nodes)
+            .attr("xlink:href", "./img/actor.png")
+            .attr("width", "20")
+            .attr("height", "20");
 });
